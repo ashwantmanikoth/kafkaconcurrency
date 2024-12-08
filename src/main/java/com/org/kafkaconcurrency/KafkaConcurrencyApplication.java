2ec72ade565223494ebb297a205acb3db7586b83
ashwantmanikoth <ashwanta75@gmail.com>
2024-12-07 20:57:05 -0400

    Topic generation, with producer mock and consumer implementation

---
+++ src/main/java/com/org/kafkaconcurrency/KafkaConcurrencyApplication.java
@@ -0,0 +1,25 @@
+package com.org.kafkaconcurrency;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaConcurrencyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaConcurrencyApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+    System.out.println("ii");
+    return args -> {
+      for (int i = 0; i < 1000; i++) {
+        kafkaTemplate.send("topic", "Test message " + i);
+      }
+    };
+  }
+}
---
+++ src/test/java/com/org/kafkaconcurrency/KafkaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.kafkaconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConcurrencyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
